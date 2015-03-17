9cb1bbb955f6dad66ac4de692abf450667cadf68
Thomas Greco <Thomasjosephgreco@yahoo.com>
2015-03-17 13:42:24 -0400

    Added a blog template with a sidebar using absolute naming with ui router. Also, Added README.md

---
+++ README.md
@@ -0,0 +1,15 @@
+##Angular Single Page Application - thomasgreco.me
+
+**Technologies In this Project**
+	* absolute naming with [ui-router](https://github.com/angular-ui/ui-router/) to create nested views for each page.
+	* The Compass framework to create 12 column grid with the Susy Grid System.
+	* Utilized jQuery in correlation with [TinyNav.js](https://github.com/viljamis/TinyNav.js) to appened the `#nav` ID selector to create a dropdown menu on viewports smaller than 768px.  
+	* Defined a schema for my contact form on the server using Mongoose with MongoDB. I then referred to this schema in angular to ensure PUT requests are issued when a form is submitted.
+
+###Next Steps:
+* Create an admin dashboard for users, to sign-in, and create a content management systems for creating posts, and other future resources.
+
+###Technologies I am thinking of using... 
+* [Firebase](https://firebase.com) & a solution similar to [FirePad](https://firepad.io) 
+* [Blogger](https://developers.google.com/blogger/docs/1.0/developers_guide_js)
+* [Swagger.io](http://swagger.io/)
--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('app', ['ui.router', 'angular-flexslider']).
+var app = angular.module('app', ['ui.router', 'angular-flexslider', 'angularModalService']).
 
 config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function($stateProvider, $urlRouterProvider, $locationProvider) {
 
@@ -7,59 +7,136 @@ config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function($s
         .state('home', {
             url: '/',
             views: {
-
-                '': {
-                    templateUrl: 'templates/home.html',
-                    controller: 'RootCtrl'
+                '' : {
+                    templateUrl: 'templates/home.html'
                 },
-
                 'Header@home': {
-                    templateUrl: 'templates/header.html',
+                    templateUrl: 'templates/Header.html',
                     controller: 'NavCtrl'
                 },
 
                 'Main-Content@home': {
-                    templateUrl: 'templates/main-content.html',
-                    controller : 'SliderCtrl'
+                    templateUrl: 'templates/Main-Content.html',
+                    controller: 'SliderCtrl'
                 },
                 'Footer@home': {
-                    templateUrl: 'templates/footer.html',
+                    templateUrl: 'templates/Footer.html',
                 }
             }
+        })
+        .state('blog', {
+            url: '/blog',
+            views: {
+                '' : {
+                    templateUrl: 'templates/blog.html'
+                },
+                'Header@blog': {
+                    templateUrl: 'templates/Header.html',
+                    controller: 'NavCtrl'
+                },
 
-        });
-}]);
+                'Blog-Content@blog': {
+                    templateUrl: 'templates/blog-content.html',
+                    controller: 'SliderCtrl'
+                },
+                'Sidebar@blog': {
+                    templateUrl: 'templates/blog-sidebar.html'
+                },
+                'Footer@blog': {
+                    templateUrl: 'templates/Footer.html',
+                }
+            }
+        })
+
+
+    //END ROUTE CONFIGURTATION
+    }]);
+      
 
 app.controller('RootCtrl', function($scope) {
     $scope.bar = [];
 
 });
 
-app.controller('NavCtrl', function($scope) {
-    $(function() {
-        $("#nav").tinyNav();
-    });
+app.controller('ModalCtrl', function($scope, ModalService) {
 
-});
+            $scope.yesNoResult = null;
+            $scope.complexResult = null;
+            $scope.customResult = null;
+
+            $scope.showYesNo = function() {
+
+                ModalService.showModal({
+                    templateUrl: "yesno/yesno.html",
+                    controller: "YesNoController"
+                }).then(function(modal) {
+                    modal.element.modal();
+                    modal.close.then(function(result) {
+                        $scope.yesNoResult = result ? "You said Yes" : "You said No";
+                    });
+                });
+
+            };
+
+            $scope.showComplex = function() {
+
+                ModalService.showModal({
+                    templateUrl: "templates/modal.html",
+                    controller: "ComplexController",
+                    inputs: {
+                        title: "A More Complex Example"
+                    }
+                }).then(function(modal) {
+                    modal.element.modal();
+                    modal.close.then(function(result) {
+                        $scope.complexResult = "Name: " + result.name + ", age: " + result.age;
+                    });
+                });
+
+            };
+
+            $scope.showCustom = function() {
+
+                ModalService.showModal({
+                    templateUrl: "custom/custom.html",
+                    controller: "CustomController"
+                }).then(function(modal) {
+                    modal.close.then(function(result) {
+                        $scope.customResult = "All good!";
+                    });
+                });
+
+            }
+        });
+
+        
+
+        app.controller('NavCtrl', function($scope) {
+            $(function() {
+                $("#nav").tinyNav();
+            });
+
+        });
+  
 
 
-app.controller('WijmoGalleryCtrl', function($scope) {
-    $scope.vm = this;
-});
-app.controller('SliderCtrl', function($scope, $timeout) {
-    $scope.slides = [
-        'images/skills/AI6.png',
-        'images/skills/cs6.png',
-        'images/skills/js-logo.png',
-        'images/skills/git.png',
-        'images/skills/html5x300.png',
-        'images/skills/yeoman.png',
-        'images/skills/nodejs350.png',
-        'images/skills/angular.png',
-        'images/skills/firebase.png',
-        'images/skills/grunt.png',
-        'images/skills/sass320x320.png',
-        'images/skills/bower.png'
 
-    ];
-});
+        app.controller('WijmoGalleryCtrl', function($scope) {
+            $scope.vm = this;
+        }); app.controller('SliderCtrl', function($scope, $timeout) {
+            $scope.slides = [
+                'images/skills/AI6.png',
+                'images/skills/cs6.png',
+                'images/skills/js-logo.png',
+                'images/skills/git.png',
+                'images/skills/html5x300.png',
+                'images/skills/yeoman.png',
+                'images/skills/nodejs350.png',
+                'images/skills/angular.png',
+                'images/skills/firebase.png',
+                'images/skills/grunt.png',
+                'images/skills/sass320x320.png',
+                'images/skills/bower.png'
+
+            ];
+        });
